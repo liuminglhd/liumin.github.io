ddc5260278c012eb2f1d567feedd0553ee9822c9
LM <425017146@qq.com>
2017-07-18 16:17:56 +0800

    我的简历

--- js/index.js
+++ js/index.js
@@ -1,22 +1,16 @@
-'use strict';
 $(function(){
-	// 图片索引值（0,1,2,3,4）
-	var num = 0;
-	// 页面加载默认第一个圆点显示蓝色
 	point(0);
-	// 存放定时器
+	var num = 0;
 	var timer = null;
-	// div.list数量
 	var count = $('div.list').length;
-	// 圆点按钮变化 定义函数
+
+	// 圆点按钮变化
 	function point(index){
 		$('.point ul li').eq(index).addClass('current').siblings().removeClass('current');
 	}
-	// ----------------------------------------------------------------------------------------------
-	// 点击右按钮
-	$('.arrow .right_btn').click(function(){
-		// 图片按顺序显示和隐藏
-		// 往右到最后一个，回到第一个
+
+	// 右按钮
+	$('.arrow .right_btn').on("click",function(){
 		if(num == count - 1){
 			$('.listbox .list').eq(0).show().siblings('.list').hide();
 			point(0);
@@ -27,11 +21,9 @@ $(function(){
 			point(num);
 		};
 	});
-	// ----------------------------------------------------------------------------------------------
-	// 点击左按钮
-	$('.arrow .left_btn').click(function(){
-		// 图片按顺序显示和隐藏
-		// 往左到第一个，回到最后一个
+
+	// 左按钮
+	$('.arrow .left_btn').on("click",function(){
 		if(num == 0){
 			$('.listbox .list').eq(count-1).show().siblings('.list').hide();
 			point(count-1);
@@ -42,8 +34,8 @@ $(function(){
 			point(num);
 		};	
 	});
-	// ----------------------------------------------------------------------------------------------
-	// 圆点显示对应图片
+
+	// 圆点显示对应div
 	$('.point ul li').on("mouseover",function(){
 		num = $(this).index();
 		$('.listbox .list').eq(num).show().siblings('.list').hide();
--- js/tab.js
+++ js/tab.js
@@ -1,27 +1,18 @@
-'use strict';
-// 技能栈
-
-
-
-
-
-
-
 // 开发项目
 var n = null;
 $('#center').show().siblings('div').hide();
-$('#centerCon').show();
 $('#center').css('margin','80px 0 0 138.6px');
+$('#centerCon').show();
 $('#center').on('mouseenter',function(){
-	$(this).css('margin','50px 0 0 10px');
-	$(this).siblings('div').fadeIn();
+	$(this).css('margin','50px 0 0 10px').siblings('div').fadeIn();
 });
+
 $('.project .hexs .hex').click(function(){
 	n = $(this).index();
 	$('.content').eq(n).show().siblings('.content').hide();
+	
 	$('.content').eq(n).on('mouseleave',function(){
-		$('#center').css('margin','80px 0 0 138.6px');
-		$('#center').siblings('div').hide();
+		$('#center').css('margin','80px 0 0 138.6px').siblings('div').hide();
 		$('.content').hide();
 		$('#centerCon').show();
 	});
